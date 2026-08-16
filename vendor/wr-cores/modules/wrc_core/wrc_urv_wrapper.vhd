3d18d855c2384557c9933d883bab88ded813f485
Lucas <109497324+t5512355123@users.noreply.github.com>
2026-08-16 12:48:46 +0800

    啟用 uRV 壓縮指令解碼

--- vendor/wr-cores/modules/wrc_core/wrc_urv_wrapper.vhd
+++ vendor/wr-cores/modules/wrc_core/wrc_urv_wrapper.vhd
@@ -192,7 +192,10 @@ begin
       g_with_hw_debug => 1,
       g_with_hw_mulh => 1,
       g_with_hw_mul => 1,
-      g_with_hw_div => 1
+      g_with_hw_div => 1,
+      -- WRPC is built with CONFIG_RISCV_COMP_INSTR=y (-march=rv32imc).
+      -- The uRV decoder must match the firmware ISA.
+      g_with_compressed_insns => 1
     )
     port map (
       clk_i            => clk_sys_i,

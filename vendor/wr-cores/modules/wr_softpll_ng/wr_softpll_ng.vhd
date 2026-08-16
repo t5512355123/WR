b094621619d204f2f7bde80f218d55426bc1613c
Lucas <109497324+t5512355123@users.noreply.github.com>
2026-08-17 02:45:54 +0800

    補齊 SoftPLL tag 診斷元件介面

--- vendor/wr-cores/modules/wr_softpll_ng/wr_softpll_ng.vhd
+++ vendor/wr-cores/modules/wr_softpll_ng/wr_softpll_ng.vhd
@@ -199,6 +199,8 @@ architecture rtl of wr_softpll_ng is
       diag_tag_valid_count_i : in std_logic_vector(31 downto 0);
       diag_trr_write_count_i : in std_logic_vector(31 downto 0);
       diag_tag_source_count_i : in std_logic_vector(31 downto 0);
+      diag_tag_ref_count_i : in std_logic_vector(31 downto 0);
+      diag_tag_feedback_count_i : in std_logic_vector(31 downto 0);
       regs_i     : in  t_spll_in_registers;
       regs_o     : out t_spll_out_registers);
   end component;

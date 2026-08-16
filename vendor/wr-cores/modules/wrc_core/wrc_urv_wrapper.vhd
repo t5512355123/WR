e302c4d2eab39b6d2a54c96bf0f007ee0b7616c4
Lucas <109497324+t5512355123@users.noreply.github.com>
2026-08-16 15:05:40 +0800

    拉開啟動標記與堆疊間距

--- vendor/wr-cores/modules/wrc_core/wrc_urv_wrapper.vhd
+++ vendor/wr-cores/modules/wrc_core/wrc_urv_wrapper.vhd
@@ -391,7 +391,7 @@ begin
         cpu_last_store_data <= dm_data_s;
         cpu_last_store_seen <= '1';
         cpu_internal_store_count <= cpu_internal_store_count + 1;
-        if dm_addr = x"0002F7F0" then
+        if dm_addr = x"0002E000" then
           cpu_boot_stage_value <= dm_data_s;
           cpu_boot_stage_seen  <= '1';
         end if;
